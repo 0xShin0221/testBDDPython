66a85712a932371fbc10ebf677a4cbebd149f8c1
sintaronettt <sintaronettt@gmail.com>
2019-07-02 13:05:25 +0900

    variable steps

--- pelican@ainoniwa.net/features/steps/example1_test.py
+++ pelican@ainoniwa.net/features/steps/example1_test.py
@@ -2,15 +2,15 @@
 from behave import *
 import socket
 
-@given(u'127.0.0.1でnginxが起動している場合')
-def step_impl(context):
-    pass
+@given(u'{host}でnginxが起動している場合')
+def step_impl(context,host):
+	context.host = host
     #raise NotImplementedError(u'STEP: Given 127.0.0.1でnginxが起動している場合')
 
-@when(u'TCPポート80にアクセスしたら')
-def step_impl(context):
+@when(u'TCPポート{port:n}にアクセスしたら')
+def step_impl(context,port):
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client.connect(("127.0.0.1", 80))
+	client.connect((context.host, port))
 	context.connection = True
     #raise NotImplementedError(u'STEP: When TCPポート80にアクセスしたら')
 
